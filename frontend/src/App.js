c514ec507eae7c26e3d02f697f211010b0a087df
E S <eriksiehr@gmx.de>
2025-06-20 15:01:33 +0200

    started with the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,51 @@
-import logo from './logo.svg';
+import logo from '../src/images/HealthSphereNew.png';
 import './App.css';
 import styled from 'styled-components';
 
 
-const Header = styled.h1`
-text-align: center;
+const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+padding: 20px;
 `
+const Header = styled.div`
+display: flex;
+padding-bottom: 10px;
+
+`
+const SideBar = styled.div`
+display: flex;
+flex-direction: column;
+gap: 10px;
+`
+const Content = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+const Dashboard = styled.div``
+
+const ImageWrapper = styled.div`
+
+`
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Header >
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-test test test        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Wrapper>
+      <Header>
+        <ImageWrapper>
+          <img src={logo} height={100}></img>
+          </ImageWrapper>
       </Header>
-    </div>
+      <SideBar>
+        <Dashboard>Patients</Dashboard>
+        <Dashboard>Doctors</Dashboard>
+        <Dashboard>Treatments</Dashboard>
+      </SideBar>
+      <Content></Content>
+    </Wrapper>
   );
 }
 
